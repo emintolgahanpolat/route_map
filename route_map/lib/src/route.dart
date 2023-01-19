1a9198488bf938c281b55b8090cec4ea19a9e5f3
Emin Tolgahan Polat <emintolgahanpolat@gmail.com>
2023-01-19 21:54:14 +0300

    pub.dev score

--- route_map/lib/src/route.dart
+++ route_map/lib/src/route.dart
@@ -1,10 +1,19 @@
+/// Marks a class as an route
 class RouteMap {
+  /// The data used to create the route.
   final String? name;
+
+  /// The `fullscreenDialog` property specifies whether the incoming route is a
+  /// fullscreen modal dialog. On iOS, those routes animate from the bottom to the
+  /// top rather than horizontally.
   final bool fullScreenDialog;
   const RouteMap({this.name, this.fullScreenDialog = false});
 }
 
+/// Marks a top-level function as an initializer function
+/// for configuring Navigator2
 class RouteMapInit {
+  /// Only files exist in provided directories will be processed
   final List<String> generateForDir;
   const RouteMapInit({
     this.generateForDir = const ['lib'],
