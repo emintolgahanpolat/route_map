57e713696e91f6052ea50135fdc869a8bfff04dd
Emin Tolgahan Polat <emintolgahanpolat@gmail.com>
2023-02-17 14:32:36 +0300

    bug fix

--- route_map/example/lib/detail.dart
+++ route_map/example/lib/detail.dart
@@ -2,12 +2,8 @@ import 'package:example/route_map.routes.dart';
 import 'package:flutter/material.dart';
 import 'package:route_map/route_map.dart';
 
-abstract class StatefullPage extends StatefulWidget {
-  const StatefullPage({super.key});
-}
-
 @RouteMap()
-class DetailPage extends StatefullPage {
+class DetailPage extends StatefulWidget {
   final String id;
   final String name;
   final bool? isShow;
